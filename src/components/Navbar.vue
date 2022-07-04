8a39999252fde6b08fb474ea2e2208b64627f51a
Alexander Bychkov <alx.bychkov@gmail.com>
2022-07-04 16:56:34 +0300

    feature: add navbar and login view.

---
+++ src/components/Navbar.vue
@@ -0,0 +1,43 @@
+<script setup>
+import { RouterLink } from "vue-router";
+defineProps({});
+</script>
+
+<template>
+  <header class="p-3 bg-dark text-white">
+    <div class="container">
+      <div
+        class="d-flex flex-wrap align-items-center justify-content-center justify-content-lg-start"
+      >
+        <ul
+          class="nav col-12 col-lg-auto me-lg-auto mb-2 justify-content-center mb-md-0"
+        >
+          <li>
+            <RouterLink to="/" class="nav-link px-2 text-white"
+              >Home</RouterLink
+            >
+          </li>
+          <li>
+            <RouterLink to="/about" class="nav-link px-2 text-white"
+              >About</RouterLink
+            >
+          </li>
+        </ul>
+
+        <div class="text-end">
+          <RouterLink to="/login" class="btn btn-outline-light me-2"
+            >Login</RouterLink
+          >
+          <RouterLink to="/register" class="btn btn-warning"
+            >Register</RouterLink
+          >
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+<style scoped>
+.router-link-active {
+  opacity: 0.5;
+}
+</style>
---
+++ src/views/LoginView.vue
@@ -0,0 +1,75 @@
+<script setup>
+import { ref } from "vue";
+
+const formData = ref({
+  email: "",
+  password: "",
+});
+
+const loginFormSubmit = () => {
+  console.log(formData.value);
+};
+</script>
+<template>
+  <main class="form-signin w-100 m-auto text-center">
+    <form>
+      <h1 class="h3 mb-3 fw-normal">Please sign in</h1>
+      <div class="form-floating">
+        <input
+          type="email"
+          class="form-control"
+          id="floatingInput"
+          placeholder="name@example.com"
+          v-model="formData.email"
+        />
+        <label for="floatingInput">Email address</label>
+      </div>
+      <div class="form-floating">
+        <input
+          type="password"
+          class="form-control"
+          id="floatingPassword"
+          placeholder="Password"
+          v-model="formData.password"
+        />
+        <label for="floatingPassword">Password</label>
+      </div>
+      <div class="checkbox mb-3 visually-hidden">
+        <label>
+          <input type="checkbox" value="remember-me" /> Remember me
+        </label>
+      </div>
+      <button
+        class="w-100 btn btn-lg btn-primary"
+        type="submit"
+        @click.prevent="loginFormSubmit"
+      >
+        Sign in
+      </button>
+    </form>
+  </main>
+</template>
+
+<style scoped>
+.form-signin {
+  max-width: 330px;
+  padding: 15px;
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+}
+.form-signin .form-floating:focus-within {
+  z-index: 2;
+}
+.form-signin input[type="email"] {
+  margin-bottom: -1px;
+  border-bottom-right-radius: 0;
+  border-bottom-left-radius: 0;
+}
+.form-signin input[type="password"] {
+  margin-bottom: 10px;
+  border-top-left-radius: 0;
+  border-top-right-radius: 0;
+}
+</style>
