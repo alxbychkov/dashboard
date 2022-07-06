2ef4885c5cb6b6f7b01fee8a7902b9cd04e59cbe
Alexander Bychkov <alx.bychkov@gmail.com>
2022-07-06 12:16:57 +0300

    fix: connection with cors

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -5,9 +5,6 @@ const instance = axios.create({
 });
 
 instance.interceptors.request.use(config => {
-    config.headers.AccessControlAllowOrigin = 'http://localhost:3000';
-    config.headers.AccessControlAllowCredentials = true;
-    config.headers.AccessControlAllowMethods = "GET, OPTIONS, POST, PUT"; 
     config.headers.Authorization = localStorage.getItem('auth') ? localStorage.getItem('auth') : '';
     return config;
 });
