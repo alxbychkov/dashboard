9b71fe7ae891b7d368aa27bef4bec6a9f55e818b
Alexander Bychkov <alx.bychkov@gmail.com>
2022-07-06 16:30:19 +0300

    feature: add content on habr page

--- src/views/HabrView.vue
+++ src/views/HabrView.vue
@@ -1,5 +1,19 @@
 <template>
-  <h1>Habr</h1>
+  <div class="h4 pb-2 mb-4 text-success border-bottom border-success">Habr</div>
+  <ul class="nav nav-pills border border-secondary p-3 rounded">
+    <li class="nav-item">
+      <a class="nav-link active" aria-current="page" href="#">Overview</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Managers</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link" href="#">Queries</a>
+    </li>
+    <li class="nav-item">
+      <a class="nav-link">Candidates</a>
+    </li>
+  </ul>
 </template>
 
 <style></style>
