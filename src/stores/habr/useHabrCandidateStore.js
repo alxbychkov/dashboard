0e122bc971b987ec7048320750a24c85ed1eafb1
Alexander Bychkov <alx.bychkov@gmail.com>
2022-07-29 16:18:22 +0300

    feature: load candidates by pages

--- src/stores/habr/useHabrCandidateStore.js
+++ src/stores/habr/useHabrCandidateStore.js
@@ -5,10 +5,26 @@ export const useHabrStore = defineStore('habrCandidateStore', {
     state: () => {
         return {
             isLoaded: false,
+            page: null,
+            pages: null,
             candidates: []
         }
     },
-    actions: {},
+    actions: {
+        async get(page = 1, limit = 10) {
+            try {
+                const response = await axios.post('candidate', {page, limit});
+        
+                this.page = response.data.candidates.page;
+                this.pages = response.data.candidates.count;
+                this.candidates = response.data.candidates.values;
+                this.isLoaded = true;
+            } catch (error) {
+                console.error('Error: ', error);
+                this.isLoaded = true;
+            }
+        }
+    },
     getters: {}
 });
 
--- src/views/habr/HabrCandidateView.vue
+++ src/views/habr/HabrCandidateView.vue
@@ -1,17 +1,16 @@
 <script setup>
-import { toRef } from "vue";
+import { toRef, ref } from "vue";
 import HabrNav from "./HabrNav.vue";
-import Loader from "../../components/Loader.vue";
 import { useHabrCandidateStore } from "../../stores/habr";
 
 const habrCandidate = useHabrCandidateStore();
-let candidates = toRef(habrCandidate, "isLoaded");
+let candidates = toRef(habrCandidate, "candidates");
+let isLoading = ref(false);
 
-const loadUserHandler = () => {
-  candidates = !candidates;
-  setTimeout(() => {
-    candidates = !candidates;
-  }, 2000);
+const loadUserHandler = async () => {
+  isLoading.value = true;
+  await habrCandidate.get();
+  isLoading.value = false;
 };
 </script>
 <template>
@@ -20,20 +19,47 @@ const loadUserHandler = () => {
   </div>
   <HabrNav />
   <div class="bord border border-secondary p-3 rounded mt-4">
+    <table
+      v-if="candidates.length"
+      class="table table-hover border rounded mb-0"
+    >
+      <thead>
+        <tr>
+          <th scope="col">#</th>
+          <th scope="col">Name</th>
+          <th scope="col">Status</th>
+          <th scope="col">Offer</th>
+          <th scope="col">Query</th>
+          <th scope="col" class="text-center">Status</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(candidate, index) in candidates" :key="candidate._id">
+          <th scope="row">{{ index + 1 }}</th>
+          <td>
+            <a :href="candidate.conversations">{{ candidate.name }}</a>
+          </td>
+          <td>{{ candidate.status }}</td>
+          <td>{{ candidate.offer }}</td>
+          <td>{{ candidate.query }}</td>
+          <td class="text-center">{{ candidate.isReceived ? "🟢" : "🔴" }}</td>
+        </tr>
+      </tbody>
+    </table>
     <div class="mt-3 d-flex justify-content-end">
       <button
         class="btn btn-success"
         type="button"
-        :disabled="candidates.isLoaded"
+        :disabled="habrCandidate.isLoaded"
         @click="loadUserHandler"
       >
         <span
-          v-if="candidates.isLoaded"
+          v-if="isLoading"
           class="spinner-border spinner-border-sm"
           role="status"
           aria-hidden="true"
         ></span>
-        {{ candidates.isLoaded ? "Loading..." : "Load users" }}
+        {{ isLoading ? "Loading..." : "Load users" }}
       </button>
     </div>
   </div>
